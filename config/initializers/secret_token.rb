14d27bd953e603c7e7b7ff1f75dda6845f1fc2a4
tourn171 <tourn171@gmail.com>
2014-09-18 03:06:52 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madman::Application.config.secret_key_base = '2eed7a85120531bbdda8383d9dcb8a71e134f8a1b22d2fee403d02a02e0b06e3e6e43763f00b32ba78f7ed6a25daf05fecf05e49cbb55f5af26ffe3df5b65c79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Madman::Application.config.session_store :cookie_store, key: '_madman_session'
